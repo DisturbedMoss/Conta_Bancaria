012b1ca7b1088b510a4aa5f72295fdc08afd67c3
DisturbedMoss <disturbedmoss35@gmail.com>
2025-09-01 15:10:32 -0300

    Adicionando as classes ContaCorrente, ContaPoupanca e Conta e os objetos

--- Menu.ts
+++ Menu.ts
@@ -1,12 +1,39 @@
 import leia = require("readline-sync")
 import { colors } from "./src/util/Colors";
+import { Conta } from "./src/model/Conta"
+import { ContaCorrente } from "./src/model/ContaCorrente";
+import { ContaPoupanca } from "./src/model/ContaPoupanca";
 
 export function main() {
     let opcao: number;
 
+    //Testes
+    const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);//Método Constructor
+    conta.visualizar();
+    conta.sacar(10500);
+    conta.visualizar();
+    conta.depositar(5000);
+    conta.visualizar();
+
+    const contacorrente: ContaCorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
+    contacorrente.visualizar();
+    contacorrente.sacar(2000);
+    contacorrente.visualizar();
+    contacorrente.depositar(1000);
+    contacorrente.visualizar();
+
+    // Objeto da Classe ContaPoupanca (teste)
+    const contapoupanca: ContaPoupanca = new ContaPoupanca(3, 123, 2, "Victor", 1000, 10);
+    contapoupanca.visualizar();
+    contapoupanca.sacar(200);
+    contapoupanca.visualizar();
+    contapoupanca.depositar(1000);
+    contapoupanca.visualizar();
+
     while (true) {
         
-        console.log(colors.bg.black, colors.fg.yellow, "*****************************************************");
+        console.log(colors.bg.black, colors.fg.yellow,
+                    "*****************************************************");
         console.log("                                                     ");
         console.log("                BANCO DO BRAZIL COM Z                ");
         console.log("                                                     ");
